47b036009b12364b8d273190bbd2936a79296241
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-09-10 13:22:47 +0100

    rake update[v4.0.0-alpha.4] and bump version

--- lib/bootstrap/version.rb
+++ lib/bootstrap/version.rb
@@ -1,4 +1,4 @@
 module Bootstrap
-  VERSION       = '4.0.0.alpha3.1'
-  BOOTSTRAP_SHA = 'c7de6107a86ebe65a6f5d5b88884256e5094ca92'
+  VERSION       = '4.0.0.alpha4'
+  BOOTSTRAP_SHA = '295c93846c154fb461f85b30e663102f7e171104'
 end
