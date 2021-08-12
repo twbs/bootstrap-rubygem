7b25c697d8e2dcf983c3a71c18faeee78e5b3db5
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-08-12 13:58:01 +0100

    Update to v5.0.2
    bundle exec rake 'update[v5.0.2]'

--- lib/bootstrap/version.rb
+++ lib/bootstrap/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Bootstrap
-  VERSION       = '5.0.1'
-  BOOTSTRAP_SHA = '58b1be927f43c779377e478df2d119f2ddf956ca'
+  VERSION       = '5.0.2'
+  BOOTSTRAP_SHA = '688bce4fa695cc360a0d084e34f029b0c192b223'
 end
