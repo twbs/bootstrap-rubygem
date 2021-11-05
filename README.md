9892831e005bdb2da72f41aa42a6391baa6ad188
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-11-05 12:49:03 +0000

    Update to v5.1.3
    bundle exec rake 'update[v5.1.3]'

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Please see the appropriate guide for your environment of choice:
 Add `bootstrap` to your Gemfile:
 
 ```ruby
-gem 'bootstrap', '~> 5.1.2'
+gem 'bootstrap', '~> 5.1.3'
 ```
 
 Ensure that `sprockets-rails` is at least v2.3.2.
--- lib/bootstrap/version.rb
+++ lib/bootstrap/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Bootstrap
-  VERSION       = '5.1.2'
-  BOOTSTRAP_SHA = '5ecef8ac010249536f92206af500f8670813c8a8'
+  VERSION       = '5.1.3'
+  BOOTSTRAP_SHA = '1a6fdfae6be09b09eaced8f0e442ca6f7680a61e'
 end
