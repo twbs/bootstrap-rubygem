c548a9a69cc1d4b833696722666c1c610cf9f4a0
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-09-18 16:24:32 +0900

    tasks/updater/js.rb: Fix popper reference

--- tasks/updater/js.rb
+++ tasks/updater/js.rb
@@ -25,7 +25,7 @@ class Updater
           'assets/javascripts/bootstrap-global-this-define.js' => <<~JS,
             // Set a `globalThis` so that bootstrap components are defined on window.bootstrap instead of window.
             window['bootstrap'] = {
-              Popper: window.Popper,
+              "@popperjs/core": window.Popper,
               _originalGlobalThis: window['globalThis']
             };
             window['globalThis'] = window['bootstrap'];
