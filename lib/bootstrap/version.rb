878e53e9c2deb53f538d1fba5d94c9434be1d62c
Timothy Carey <timcarey1989@gmail.com>
2023-01-25 13:34:03 -0500

    update bs version

--- lib/bootstrap/version.rb
+++ lib/bootstrap/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Bootstrap
-  VERSION       = '5.2.3'
-  BOOTSTRAP_SHA = 'cb021439c683d9805e2864c58095b92d405e9b11'
+  VERSION       = '5.3.0'
+  BOOTSTRAP_SHA = 'GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD'
 end
