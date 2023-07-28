3968ff3f24aee919943fb44eb66c12e2397375b1
Wolfgang Tom <mail@wolfgangtom.de>
2023-07-28 10:31:43 +0200

    update to bootstrap 5.3.1

--- lib/bootstrap/version.rb
+++ lib/bootstrap/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Bootstrap
-  VERSION       = '5.3.0.alpha3'
-  BOOTSTRAP_SHA = 'ac576614a5515e429f27e756fad81d5aa05e95a6'
+  VERSION       = '5.3.1'
+  BOOTSTRAP_SHA = 'c81a694ff3363c37b5ac8f8dbcb3f6ce90d350dd'
 end
