ef54e895dc9b5666d41c1277b347a6daae654bbf
Gleb Mazovetskiy <glex.spb@gmail.com>
2024-04-29 09:37:13 +0900

    Update to v5.3.3

--- lib/bootstrap/version.rb
+++ lib/bootstrap/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Bootstrap
-  VERSION       = '5.3.2'
-  BOOTSTRAP_SHA = '344e912d04b5b6a04482113eff20ab416ff01048'
+  VERSION       = '5.3.3'
+  BOOTSTRAP_SHA = '6e1f75f420f68e1d52733b8e407fc7c3766c9dba'
 end
